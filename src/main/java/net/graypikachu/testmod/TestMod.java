030387182d5795478d40625c25c1befaee72d62d
GrayPikachu <Leshagorbel06@gmail.com>
2024-11-07 15:47:37 +0300

    TEST2

--- src/main/java/net/graypikachu/testmod/TestMod.java
+++ src/main/java/net/graypikachu/testmod/TestMod.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-//Very important comment
+//Very important comment1212121
 public class TestMod implements ModInitializer {
 	public static final String MOD_ID = "testmod";
 
